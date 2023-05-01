42789d0b14c67601f76d41141123c57a2a0e271c
Otis Sutton <otis@Clares-MacBook-Pro.local>
2023-05-01 19:26:33 +1200

    Add fake accordion data

--- apps/components.stitches.design/src/components/accordion.tsx
+++ apps/components.stitches.design/src/components/accordion.tsx
@@ -1,13 +1,19 @@
 import { h as _ } from 'preact';
 
+import { faker } from '@faker-js/faker';
 import { Accordion as HeadlessAccordion } from 'components';
 
-export const Accordion = () => {
-  const items = [{
-    name: 'Item One',
-    value: 1
-  }]
+const createAccordion = () => {
+  return {
+    id: faker.datatype.uuid(),
+    name: faker.internet.userName(),
+    value: faker.lorem.paragraph()
+  };
+}
 
+const items = Array.from({ length: 10 }).map(createAccordion);
+
+export const Accordion = () => {
   return (
     <HeadlessAccordion items={items}>
       {({ item, meta }) => (
