b9433e952b1781572e7cc2f2a3838b91071d4db7
Otis Sutton <chopfitzroy@gmail.com>
2023-08-07 19:51:58 +1200

    Add tabs for preview and console

--- apps/exo-ui.dev/src/components/Editor.tsx
+++ apps/exo-ui.dev/src/components/Editor.tsx
@@ -1,7 +1,7 @@
 import type { ComponentRef } from 'react';
 import type { SandpackFiles } from "@codesandbox/sandpack-react";
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { cyberpunk } from "@codesandbox/sandpack-themes";
 import { NoSymbolIcon, ArrowPathIcon, CodeBracketIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
 
@@ -14,8 +14,11 @@ import {
 	UnstyledOpenInCodeSandboxButton,
 } from "@codesandbox/sandpack-react";
 
+type Tabs = 'preview' | 'console';
+
 interface EditorProps {
 	dir: string;
+	title: string;
 	files: SandpackFiles;
 }
 
@@ -36,12 +39,20 @@ const RefreshButton = () => {
 // @NOTE
 // - Heavily inspired by:
 // - https://www.joshwcomeau.com/react/next-level-playground/
-export const Editor = ({ dir, files }: EditorProps) => {
+export const Editor = ({ dir, title, files }: EditorProps) => {
 	// @NOTE
 	// - https://github.com/codesandbox/sandpack/issues/730
 	const consoleRef = useRef<ComponentRef<typeof SandpackConsole>>(null);
 
+	const [activeTab, setActiveTab] = useState<Tabs>('preview');
+
 	const link = `https://github.com/chopfitzroy/exo-ui/tree/main/apps/exo-ui.dev/src/examples/${dir}`;
+
+	const isPreviewActive = activeTab === 'preview';
+	const isConsoleActive = activeTab === 'console';
+
+	const handlePreviewTab = () => setActiveTab('preview');
+	const handleConsoleTab = () => setActiveTab('console');
 
 	const handleClear = () => {
 		if (consoleRef.current !== null) {
@@ -62,28 +73,43 @@ export const Editor = ({ dir, files }: EditorProps) => {
 				}}
 			>
 				<div className="playground-controls">
-					<a href={link} title="View on GitHub" target="_blank" className="playground-button">
-						<CodeBracketIcon className="playground-icon" />
-					</a>
-					<UnstyledOpenInCodeSandboxButton className="playground-button">
-						<ArrowTopRightOnSquareIcon className="playground-icon" />
-					</UnstyledOpenInCodeSandboxButton>
+					<p>{title}</p>
+					<div className="playground-external">
+						<a href={link} title="View on GitHub" target="_blank" className="playground-button">
+							<CodeBracketIcon className="playground-icon" />
+						</a>
+						<UnstyledOpenInCodeSandboxButton className="playground-button">
+							<ArrowTopRightOnSquareIcon className="playground-icon" />
+						</UnstyledOpenInCodeSandboxButton>
+					</div>
 				</div>
 				<div className="playground-editor">
 					<SandpackCodeEditor />
 				</div>
 				<div className="playground-controls">
-					<button type="button" onClick={handleClear}>
-						<NoSymbolIcon className="playground-icon" />
-					</button>
-					<RefreshButton />
+					<div className="playground-tabs">
+						<button onClick={handlePreviewTab} className={`playground-tab ${isPreviewActive ? 'active' : ''}`}>Preview</button>
+						<button onClick={handleConsoleTab} className={`playground-tab ${isConsoleActive ? 'active' : ''}`}>Console</button>
+					</div>
+					{isPreviewActive && (
+						<RefreshButton />
+					)}
+					{isConsoleActive && (
+						<button type="button" onClick={handleClear}>
+							<NoSymbolIcon className="playground-icon" />
+						</button>
+					)}
 				</div>
-				<div className="playground-preview">
-					<SandpackPreview
-						showRefreshButton={false}
-						showOpenInCodeSandbox={false}
-					/>
-					<SandpackConsole ref={consoleRef} />
+				<div className="playground-outputs">
+					<div className={`playground-preview ${isPreviewActive ? 'active' : ''}`}>
+						<SandpackPreview
+							showRefreshButton={false}
+							showOpenInCodeSandbox={false}
+						/>
+					</div>
+					<div className={`playground-console ${isConsoleActive ? 'active' : ''}`}>
+						<SandpackConsole ref={consoleRef} />
+					</div>
 				</div>
 			</SandpackProvider>
 		</div>
